6e6a38154c493ac513bf9ba3d4e8e1f2cd5d9417
prabhunath111 <prabhutiwari111@gmail.com>
2021-03-19 12:14:51 +0530

    added google map screen

--- lib/screens/map/map_screen.dart
+++ lib/screens/map/map_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -6,11 +9,34 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  GoogleMapController mapController;
+  Completer<GoogleMapController> _controller = Completer();
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(12.9061, 77.5845),
+    zoom: 7,
+  );
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: _onMapCreated,
+            trafficEnabled: true,
+            // myLocationEnabled: true,
+            // myLocationButtonEnabled: true,
+            markers: Set<Marker>.of(markers.values),
+          ),
+        ],
+      ),
     );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    _controller.complete(controller);
   }
 }
