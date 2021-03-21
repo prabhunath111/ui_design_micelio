b1a34a5da5f3f14399f586d2d3c76cce379d3d01
Prabhu Nath Tiwary <prabhunt@micelio.local>
2021-03-21 21:14:30 +0530

    fixed login card scroll

--- lib/screens/otp/otp.dart
+++ lib/screens/otp/otp.dart
@@ -13,62 +13,66 @@ class _OtpScreenState extends State<OtpScreen> {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: screenHeight,
-          width: screenWidth,
-          color: Color(0xFFC4C6C8),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(
-                height: screenHeight * 0.2,
-              ),
-              Image.asset('assets/images/png2.png', width: 72,height: 72),
-              SizedBox(
-                height: screenHeight * 0.02,
-              ),
-              Text(
-                'Enter OTP',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.normal,
-                    letterSpacing: 1.0),
-              ),
-              SizedBox(
-                height: screenHeight * 0.01,
-              ),
-              Text(
-                'Please enter your OTP',
-                style: TextStyle(
-                    color: Colors.black45, fontSize: 16.0, letterSpacing: 1.0),
-              ),
-              SizedBox(
-                height: screenHeight * 0.14,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                child: Card(
-                  child: Container(
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(8.0, 24.0, 8.0, 24.0),
-                      child: Column(
-                        children: [
-                          TextFieldOtp(),
-                          SizedBox(
-                            height: 8.0,
-                          ),
-                          SubmitOtp()
-                        ],
-                      ),
+      body: Stack(
+        children: [
+          Container(
+            height: screenHeight,
+            width: screenWidth,
+            color: Color(0xFFC4C6C8),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: screenHeight * 0.2,
+                ),
+                Image.asset('assets/images/png2.png', width: 72,height: 72),
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+                Text(
+                  'Enter OTP',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.normal,
+                      letterSpacing: 1.0),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.01,
+                ),
+                Text(
+                  'Please enter your OTP',
+                  style: TextStyle(
+                      color: Colors.black45, fontSize: 16.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: screenHeight * 0.1,
+            left: 0.0,
+            right: 0.0,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+              child: Card(
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 24.0, 8.0, 24.0),
+                    child: Column(
+                      children: [
+                        TextFieldOtp(),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        SubmitOtp()
+                      ],
                     ),
                   ),
                 ),
-              )
-            ],
-          ),
-        ),
+              ),
+            ),
+          )
+        ],
       )
     );
   }
